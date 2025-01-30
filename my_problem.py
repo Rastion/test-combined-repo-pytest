d17ac0ba831cc4630338261999c28462592346fb
leonidas1312 <jonhkarystos@gmail.com>
2025-01-30 15:45:26 +0200

    Add solver & problem code & config

---
+++ my_problem.py
@@ -0,0 +1,4 @@
+from rastion_core.problems.traveling_salesman import TSPProblem
+
+class MyProblem(TSPProblem):
+    pass
---
+++ my_solver.py
@@ -0,0 +1,4 @@
+from rastion_core.algorithms.genetic_algorithm import GeneticAlgorithm
+
+class MySolver(GeneticAlgorithm):
+    pass
